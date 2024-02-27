897ced49093ba9d68cd85f3ebdf3fc45ee18850e
atalaykarahan <krhnatalay@gmail.com>
2024-02-27 20:28:04 +0300

    kullancının kitaplarım sayfası için gerekli olan endpoint eklendi

--- src/controller/reading.ts
+++ src/controller/reading.ts
@@ -1,19 +1,32 @@
 import { RequestHandler } from "express";
 import ReadingModel from "../models/reading";
+import BookModel from "../models/book";
+import PubliserModel from "../models/publisher";
+import AuthorModel from "../models/author";
+import StatusModel from "../models/status";
 import createHttpError from "http-errors";
 
 export const getMyReading: RequestHandler = async (req, res, next) => {
-    try {
+  try {
+    const user_id = req.session.user_id;
 
-        const user_id = req.session.user_id
+    const myReadings = await ReadingModel.findAll({
+      attributes: ["reading_id"],
+      where: { user_id: user_id },
+      include: [
+        {
+          model: BookModel,
+          attributes: ["book_id", "book_title", "image_path"],
+          include: [{ model: PubliserModel }, { model: AuthorModel }],
+        },
+        { model: StatusModel },
+      ],
+    });
+    if (!myReadings) {
+      throw createHttpError(404, "You have no reading record");
+    }
 
-        const myReadings = await ReadingModel.findAll({ where: { user_id: user_id } });
-        if(!myReadings){
-            throw createHttpError(404, "You have no reading record");
-        }
-
-        res.status(200).json(myReadings);
-
+    res.status(200).json(myReadings);
 
     //   const books = await BookModel.findAll({
     //     attributes: ["book_id", "book_title", "book_summary"],
@@ -23,9 +36,9 @@ export const getMyReading: RequestHandler = async (req, res, next) => {
     //       { model: StatusModel },
     //     ],
     //   });
-  
+
     //   res.status(200).json(books);
-    } catch (error) {
-      next(error);
-    }
-  };
+  } catch (error) {
+    next(error);
+  }
+};
