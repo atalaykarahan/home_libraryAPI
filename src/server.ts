24ac49f9e2cd67dedbf15aeb77a2019beeaad397
atalaykarahan <krhnatalay@gmail.com>
2024-04-20 22:43:17 +0300

    secure true yapılandırıldı

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       sameSite : "none",
-      secure: false,
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     },
     rolling: true,
