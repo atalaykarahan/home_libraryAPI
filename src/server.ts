3cc23bc692c80af036531c343151a4db77841d9a
atalaykarahan <krhnatalay@gmail.com>
2024-04-21 09:03:14 +0300

    secure true yapınca hiç kaydedilmiyor mecbur false olmak zorunda

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       sameSite : "strict",
-      secure: true,
+      secure: false,
       domain: "atalaykarahan.com",
       maxAge: 24 * 60 * 60 * 1000,
     },
