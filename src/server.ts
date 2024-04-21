1bda510cb057904c9b8eeb84bb3ccceaf239cc6b
atalaykarahan <krhnatalay@gmail.com>
2024-04-21 08:37:19 +0300

    secure false ve samesite strict iken

--- src/server.ts
+++ src/server.ts
@@ -53,7 +53,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      sameSite : "none",
+      sameSite : "strict",
       secure: false,
       maxAge: 24 * 60 * 60 * 1000,
     },
