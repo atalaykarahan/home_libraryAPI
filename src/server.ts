1dfb10938b5ea55f6c20b87288eadcab98fb1f3e
atalaykarahan <krhnatalay@gmail.com>
2024-04-21 09:01:56 +0300

    secure kısmı true yapıldı

--- src/server.ts
+++ src/server.ts
@@ -54,7 +54,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       sameSite : "strict",
-      secure: false,
+      secure: true,
       domain: "atalaykarahan.com",
       maxAge: 24 * 60 * 60 * 1000,
     },
