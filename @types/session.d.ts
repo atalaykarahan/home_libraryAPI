453d03ac1122297cb5d4845e2b68a060d24496f2
root <root@89.213.56.112>
2024-08-17 17:34:16 +0000

    change some versions for pm2

--- @types/session.d.ts
+++ @types/session.d.ts
@@ -1,8 +1,8 @@
 import { DataTypes } from "sequelize";
 
 declare module "express-session"{
-    export interface SessionData{
-        user_id: DataTypes.BIGINT;
+      export interface SessionData{
+	  user_id:any;	
         user_authority_id:DataTypes.BIGINT;
     }
 }
